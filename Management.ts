b1fe75e11afc737cf000f5f5086ea97a2156ad03
abhigandhi29 <abhigandhi29@gmail.com>
2021-04-02 17:59:30 +0530

    adding nvm and corecting Management.ts

--- Management.ts
+++ Management.ts
@@ -1,27 +1,30 @@
-class Maanagement{
-    instance = null;
-    static Application = []
+class Management{
+    static instance = null;
+    static Application = [];
     stationList = [];
     trainList = [];
     private constructor(){
     }
-    getInsstance(){
+    static getInstance() : Management{
         if (this.instance==null){
-            this.instance = new Maanagement();
+            this.instance = new Management();
         }
         return this.instance;
     }
-    addStation(Station){
+    addStation(Station) : void{
         this.stationList.push(Station);
     }
-    removeStation(Station){
+    removeStation(Station) : void{
         this.stationList.splice(this.stationList.indexOf(Station));
     }
-    addTrain(Train){
+    addTrain(Train) : void{
         this.trainList.push(Train);
     }
-    removeTrain(Train){
+    removeTrain(Train) : void{
         this.trainList.slice(this.trainList.indexOf(Train));
     }
 
 }
+var m = Management.getInstance();
+m.trainList = []
+console.log('h')
