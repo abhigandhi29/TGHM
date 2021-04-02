cdf0cfc854afb7888164ddd3a1fc79bf819fdf85
abhigandhi29 <abhigandhi29@gmail.com>
2021-04-02 20:16:35 +0530

    ..

---
+++ Menu.ts
@@ -0,0 +1,11 @@
+export class Menu{
+    private __Items = [];
+    constructor(){
+    }
+    getMenuItems(){
+        return this.__Items;
+    }
+    getPrice(s:string){
+        
+    }
+}
--- Restaurant.ts
+++ Restaurant.ts
@@ -1,5 +1,19 @@
-class Restaurant extends Account{
-    constructor(self, name,password) {
+import {Account} from "./Account"
+import {AccountType} from "./Enum"
+import {Menu} from "./Menu"
+import {Management} from "./Management"
+export class Restaurant extends Account{
+    orderlist = [];
+    private __agent = [];
+    private __agentStatus = [];
+    accetanceStatus = false;
+    Menu = new Menu();
+    certi = [];
 
+
+    constructor(name,password) {
+        super(name,new Date(),AccountType.Restaurant,password);
+        Management.Application.push(this);
     }
+    
 }
