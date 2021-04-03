1a565b9be0c6e409d4fab4e4c41157ab747de718
abhigandhi29 <abhigandhi29@gmail.com>
2021-04-04 03:39:39 +0530

    ..

--- Item.ts
+++ Item.ts
@@ -1,11 +1,14 @@
 import {FoodType} from "./Enum"
+import { Restaurant } from "./Restaurant";
 export class Item{
     name: string;
     price: number;
     type : string;
-    constructor(name : string,price : number,type : number){
+    restaurant : Restaurant;
+    constructor(name : string,price : number,type : number,restaurant : Restaurant){
         this.name = name;
         this.price = price;
         this.type = FoodType[type];
+        this.restaurant = restaurant;
     }
 }
