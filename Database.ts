72083930f2ad36761f5aaf2b5a6c021154833d76
Shashvat Gupta <shashvat6264@gmail.com>
2021-04-04 20:45:08 +0530

    Database fetch get complete

--- Database.ts
+++ Database.ts
@@ -1,4 +1,8 @@
 import {Management} from "./Management";
+import {Customer} from "./Customer";
+import {Restaurant} from "./Restaurant";
+import {Station} from "./Station";
+import {Train} from "./Train";
 
 const fs = require("fs");
 
@@ -17,16 +21,34 @@ export class Database{
 
     static writeState(){
           try {
-            fs.writeFileSync("./data/Customer.json", JSON.stringify(Management.Customers))
+              fs.writeFileSync("./data/Customers.json", JSON.stringify(Management.Customers))
+              fs.writeFileSync("./data/Restaurants.json", JSON.stringify(Management.Application))
+              fs.writeFileSync("./data/Stations.json", JSON.stringify(Management.stationList))
+              fs.writeFileSync("./data/Trains.json", JSON.stringify(Management.trainList))
           } catch (err) {
-            console.error(err)
+              console.error(err)
           }
     }
 
 
     static readState(){
         try {
-            Management.Customers = JSON.parse(fs.readFileSync("./data/Customers.json", 'utf8'));
+            let arrCust = JSON.parse(fs.readFileSync("./data/Customers.json", 'utf8'));
+            for (let i=0;i<arrCust.length;i++){
+                Management.Customers.push(Object.setPrototypeOf(arrCust[i], Customer.prototype));
+            }
+            arrCust = JSON.parse(fs.readFileSync("./data/Restaurants.json", 'utf8'));
+            for (let i=0;i<arrCust.length;i++){
+                Management.Application.push(Object.setPrototypeOf(arrCust[i], Restaurant.prototype));
+            }
+            arrCust = JSON.parse(fs.readFileSync("./data/Stations.json", 'utf8'));
+            for (let i=0;i<arrCust.length;i++){
+                Management.stationList.push(Object.setPrototypeOf(arrCust[i], Station.prototype));
+            }
+            arrCust = JSON.parse(fs.readFileSync("./data/Trains.json", 'utf8'));
+            for (let i=0;i<arrCust.length;i++){
+                Management.trainList.push(Object.setPrototypeOf(arrCust[i], Train.prototype));
+            }
         } catch (err) {
             console.error(err)
             return false
